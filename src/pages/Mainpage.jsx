37fdcf3e78be241125d96654681e0047076abb21
krishshah1108 <krishhshah1108@gmail.com>
2024-11-27 13:10:30 +0530

    almost completed

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -3,12 +3,35 @@ import "../css/Mainpage.css";
 import Box from "../components/Box";
 const Mainpage = () => {
   const [toShowRules, setToShowRules] = useState(false);
-  const numArr = [1, 2, 3, 4, 5, 6];
-  const [score, setScore] = useState(4);
+  const [score, setScore] = useState(0);
   const [selectedNumber, setSelectedNumber] = useState();
-  const selectHandler = (value) => {
-    setSelectedNumber(value);
+  const [imageNum, setImageNum] = useState(1);
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const selectHandler = (value) => setSelectedNumber(value);
+  const randomNum = () => Math.floor(Math.random() * 6) + 1;
+  const rollDice = () => {
+    if (selectedNumber === undefined) {
+      alert("Select any number");
+      return;
+    }
+    setIsSpinning(true);
+    setTimeout(() => {
+      setImageNum(randomNum());
+      if (selectedNumber == imageNum) {
+        setScore((prev) => prev + selectedNumber);
+        setSelectedNumber(undefined);
+        // console.log(score);
+      } else {
+        setScore((prev) => prev - Math.floor(selectedNumber / 2));
+        setSelectedNumber(undefined);
+      }
+      setIsSpinning(false);
+    }, 500);
   };
+
+  const numArr = [1, 2, 3, 4, 5, 6];
+
   return (
     <div className='conatiner'>
       <header>
@@ -35,14 +58,18 @@ const Mainpage = () => {
               <p className='selected'>
                 Your guess is: <span>{selectedNumber}</span>
               </p>
-            ) : (
-              <p className='err-msg'>Please select number</p>
-            )}
+            ) : null}
           </div>
         </div>
       </header>
       <div className='main-body'>
-        <img src='/dice_1.png' alt='' srcset='' />
+        <img
+          className={`dice ${isSpinning ? "spinning" : ""}`}
+          src={`/dice_${imageNum || 1}.png`}
+          alt=''
+          srcset=''
+          onClick={() => rollDice()}
+        />
         <p>Click on dice to roll</p>
         <button onClick={() => setScore(0)}>Reset Score</button>
         <button onClick={() => setToShowRules(!toShowRules)}>Show rules</button>
@@ -53,7 +80,7 @@ const Mainpage = () => {
           Select any number <br />
           Create on dice image <br />
           After click on dice , if selected number is equal to dice number you
-          will get same points! If your guess is wrong 2 points will be
+          will get same points! If your guess is wrong half points of it will be
           dedcuted!
         </p>
       </div>
